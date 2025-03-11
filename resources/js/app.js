58be45065c00aee8f3da0f295db603619a992b21
Adam Hamzah Nurwahyudi <adamhamzahofficial13@gmail.com>
2025-03-11 21:58:15 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+import Swal from 'sweetalert2';
+window.Swal = Swal;
